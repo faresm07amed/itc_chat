a6c5a6b8eda6587c9d965a4f8359527d94d8f2c1
Fares <faresmohamed523@gmail.com>
2026-03-10 18:33:01 +0200

    main setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:itc_chat/core/config/app_theme.dart';
+import 'package:itc_chat/features/chat/ui/screens/chat_screen.dart';
+import 'package:itc_chat/features/profile/ui/screens/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +12,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ITC Ai Chat',
-      debugShowCheckedModeBanner: false,
-      // theme: AppTheme.lightTheme,
-      // darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      // home: const HomeScreen(),
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: themeNotifier,
+      builder: (_, ThemeMode currentMode, __) {
+        return MaterialApp(
+          title: 'ITC Ai Chat',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: currentMode,
+          home: Chat2(),
+        );
+      },
     );
   }
 }
